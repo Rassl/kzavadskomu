159e98a950628cecccc1bee42213fad08e471cfd
Rasul <rasul.sahibjanli@gmail.com>
2016-10-15 19:43:58 +0300

    add constructor

---
+++ enemyConstructor.js
@@ -0,0 +1,22 @@
+
+// var Entity = function (url, pos, size, speed, frames, dir, once) {
+// 	this.url = url;
+// 	this.pos = pos;
+// 	this.size = size;
+// 	this.speed = speed;
+// 	this.frames = frames;
+// 	this.dir = dir;
+// 	this.once = once;
+	
+// 	var abstractEntity = {};
+	
+// 	abstractEntity.getSptrite = new Sprite(this.url, this.pos, this.size, this.speed, this.frames, this.dir, this.once);
+
+// 	return abstractEntity;
+// } 
+
+// var player = new Entity('images/mansprite.png', [0, 0], [69, 130]);
+// console.log(player);
+//gang of four
+//stefanov
+//slideshare
